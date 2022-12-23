34024d6868655a11675d4b79a54370044ef88afc
Anton Rotshtein <rotshtein.anton@gmail.com>
2022-12-23 23:39:05 +0200

    added client api

---
+++ client/src/components/api/api.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+const API = axios.create({
+    baseURL: 'http://webbox.net',
+    withCredentials: false,
+    headers: {
+        'Content-Type': 'text/plain',
+    },
+    timeout: 30000,
+    responseType: 'json',
+});
+
+API.interceptors.response.use(
+    res => {
+        return res;
+    },
+    error => {
+        return Promise.reject(error);
+    },
+);
+
+class Request {
+
+    get = async (url) => {
+        await API.get(url)
+    }
+
+    post = async (url, data) => {
+        return await API.post(url, data)
+    }
+
+    put = async (url, data) => {
+        return await API.put(url, data)
+    }
+
+    remove = async (url, data) => {
+        return await API.delete(url, data)
+    }
+}
+
+export default new Request();
